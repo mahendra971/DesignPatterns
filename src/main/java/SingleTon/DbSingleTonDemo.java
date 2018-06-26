81f6f9614825b59a8ac3271a2c5a4bda7415ecf9
MahendraBabuIragala <mahendra.971@gmail.com>
2018-06-26 15:48:40 +0530

    Builder and Singleton ThreadSafe

--- src/main/java/SingleTon/DbSingleTonDemo.java
+++ src/main/java/SingleTon/DbSingleTonDemo.java
@@ -10,6 +10,13 @@ public class DbSingleTonDemo {
         System.out.println(ob1.hashCode());
         
         System.out.println("Testing Git");
+        
+        ThreadSafeSingleTon tob = ThreadSafeSingleTon.getInstance();
+        System.out.println(tob.hashCode());
+        System.out.println(tob);
+        ThreadSafeSingleTon tob1 = ThreadSafeSingleTon.getInstance();
+        System.out.println(tob1.hashCode());
+        System.out.println(tob1);
 
     }
 }
